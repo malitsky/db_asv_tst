46aa27f78a85cf9a562d29870b90fdbcc4eb8504
nmalitsky <nmalitsky@gmail.com>
2019-05-12 19:07:22 -0400

    minor changes

--- asv/benchmarks/benchmarks.py
+++ asv/benchmarks/benchmarks.py
@@ -4,10 +4,6 @@
 import time as ttime
 import uuid
 
-# from databroker.headersource.mongo import MDS
-# from databroker.utils import create_test_database
-# from databroker.mongo import Registry
-
 from databroker.tests.utils import build_pymongo_backed_broker
 from databroker.tests.test_mds import (setup_syn, syn_data)
 
@@ -22,12 +18,11 @@ class DatabrokerSuite:
 
     db = None
     mds_all = None
-	
+
     def setup(self):
         request = Request()
         self.db = build_pymongo_backed_broker(request)
         self.mds_all = self.db.mds
-
 
     def time_bulk_insert(self):
         mdsc = self.mds_all
@@ -37,15 +32,3 @@ class DatabrokerSuite:
 
         mdsc.bulk_insert_events(e_desc, all_data, validate=False)
         mdsc.insert_run_stop(rs, ttime.time(), uid=str(uuid.uuid4()))
-
-        '''
-        ev_gen = mdsc.get_events_generator(e_desc)
-
-        for ret, expt in zip(ev_gen, all_data):
-            assert ret['descriptor'] == e_desc
-            for k in ['data', 'timestamps', 'time', 'uid', 'seq_num']:
-                print(k, ret[k])
-                assert ret[k] == expt[k]
-            assert ret['filled'] == {'Z': False}
-        '''
-
